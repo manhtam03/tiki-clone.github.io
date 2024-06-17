138ba4b2563d005f78ee5164692778e2a537c884
manh tam <manhtam96@gmail.com>
2024-06-17 23:46:07 +0700

    update giao dien

--- js/home_page.js
+++ js/home_page.js
@@ -9,26 +9,6 @@ function showLoading() {
 
 
 //menu tab
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("nhap-khau");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("content");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "flex";
-    evt.currentTarget.className += " active";
-  }
 
 //button prev and next slide
 document.addEventListener('DOMContentLoaded', function() {
